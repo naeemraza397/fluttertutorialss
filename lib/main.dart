51add51e603d0f808fa08f68c318efa08f0e435c
naeemraza397 <naeemraza397@gmail.com>
2021-09-01 17:12:56 +0500

    videp56

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/screens/Hom_page.dart';
 import 'package:flutter_catalog/screens/Login_page.dart';
-
+int name = 5;
 void main() {
   runApp(Myapp());
 }
 
 class Myapp extends StatelessWidget {
+   
   const Myapp({Key? key}) : super(key: key);
-
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
